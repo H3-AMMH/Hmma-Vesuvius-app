f09707db81460d6c788ea836f0c2fdd2690b7862
half0150 <half0150@edu.sde.dk>
2025-09-05 09:25:26 +0200

    Made some small changes(Fixes)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:cafe/views/waiter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'views/chef.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   runApp(const MyApp());
 }
 
--- lib/views/chef.dart
+++ lib/views/chef.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import '../viewmodels/chef_view_model.dart';
 import '../models/menu_item.dart';
 
-void main() => runApp(const ChefApp());
-
 class ChefApp extends StatelessWidget {
   const ChefApp({super.key});
 
@@ -54,7 +52,7 @@ class _ChefPageState extends State<ChefPage> {
         return const Center(child: CircularProgressIndicator());
       }
       if (_menuItems.isEmpty) {
-        return const Center(child: Text("No menu items found"));
+        return const Center(child: Text("Ingen retter fundet"));
       }
       return ListView.builder(
         itemCount: _menuItems.length,
